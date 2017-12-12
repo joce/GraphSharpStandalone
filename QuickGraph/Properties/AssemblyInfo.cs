1d9e3f2bf16090e0d6d503ecd7787c2f7854dc99
joce <jocelynlegault@gmail.com>
2017-12-12 11:08:37 -0500

    First commit
    GraphSharp + QuickGraph (3.6) sources. Builds ok.
    
    Signed-off-by: joce <joce@unity3d.com>

---
+++ QuickGraph/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("QuickGraph")]
+[assembly: AssemblyDescription("Generic Graph Data Strutures and Algorithms for .NET")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Jonathan Peli de Halleux")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("Copyright http://quickgraph.codeplex.com/")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyDelaySign(false)]
+[assembly: AssemblyKeyName("")]
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("70D6E9FC-67DA-4AFE-87B1-E88795CA18C8")]
+
+[assembly: AssemblyVersion("3.6.0.0")]
+[assembly: AssemblyFileVersion("3.6.0.0")]
